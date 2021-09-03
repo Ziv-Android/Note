fc2d92b536ee5798b10a65b9726a63f52a4ad5b3
zbx-zp <zhaopeng@mustint.com>
2021-09-03 18:32:44 +0800

    feat: update

--- 1_android/java/code/HelloWorld.java
+++ 1_android/java/code/HelloWorld.java
@@ -1,9 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         float random = (float) (Math.round(Math.random() * 20)) / 100;
-        System.out.print("Hello world" + random);
+        System.out.println("Hello world" + random);
 
         float tem = 10.01F * 10 % 10;
-        System.out.print("Test: " + tem);
+        System.out.println("Test: " + tem);
+
+        float time = System.currentTimeMillis();
+        int timeS = (int) (time / 60000);
+        System.out.println("Test: " + String.valueOf(time) + " -> " + timeS);
     }
 }
