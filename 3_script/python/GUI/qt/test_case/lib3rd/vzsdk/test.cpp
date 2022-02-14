466449f6ac0cc8044cb1cd2812ad14c8e6ed455e
zbx-zp <zhaopeng@mustint.com>
2022-02-14 18:46:10 +0800

    feat: python 更新

--- 3_script/python/GUI/qt/test_case/lib3rd/vzsdk/test.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-
-
-int foo(int a, int b) {
-  printf("you input %d and %d\n", a, b);
-  return a + b;
-}
--- 3_script/python/GUI/qt/test_case/oqc_tool/__init__.py
+++
@@ -1,4 +0,0 @@
-import ctypes
-
-# 调用VZSDK
-objdll = ctypes.cdll.LoadLibrary('./lib3rd/vzsdk/VzLPRSDK.dll')
